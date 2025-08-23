d42b866ca6bcd7cba01f1aa80c13287e1315bd79
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-08-23 19:52:19 +0100

    Add SignUp component with form and navigation to Login page

--- src/components/Verification.jsx
+++ src/components/Verification.jsx
@@ -1,6 +1,22 @@
+import { NavLink } from "react-router-dom"
+
 export default function SignUpPage() {
     return (
-        <div>hello sign up pleas</div>
+        <div className="sign-in">
+            <div className="sign-in-bg"></div>
+            <div className="sign-in-txt">
+                <h1>Sign Up</h1>
+                <form>
+                    <input type="text" />
+                    <input type="text" />
+                    <input type="text" />
+                    <input type="text" />
+                    <input type="text" />
+                    <p>Already on LauTalk? <NavLink to="/login">Login</NavLink></p>
+                    <button>Sign Up Now!!!</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
